0af77754125ffde3ae33744ccc5f8137b4730d91
diogo8plays-art <diogo8plays@gmail.com>
2026-06-02 09:40:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const elements = document.querySelectorAll(".card, .gallery img");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+}, { threshold: 0.2 });
+
+elements.forEach(el => {
+    el.style.opacity = 0;
+    el.style.transform = "translateY(30px)";
+    el.style.transition = "0.8s";
+    observer.observe(el);
+});
